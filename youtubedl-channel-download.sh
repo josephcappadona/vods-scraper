5dff4e26fc70cb99550f74044949ede75f553880
Joseph Cappadona <josephcappadona27@gmail.com>
2018-05-01 04:08:22 -0400

    changed variable declaration format

--- youtubedl-channel-download.sh
+++ youtubedl-channel-download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export NAME=$1
-export URL=$2
-export COMMAND='youtube-dl --write-description --write-info-json --write-sub --all-subs --sub-format "srt/best" --write-thumbnail --sleep-interval 1 --retries 30 -o "/media/serverhdd3/$NAME/%(playlist)s/%(title)s.%(ext)s" -f best -ciw "$URL"'
+NAME=$1
+URL=$2
+COMMAND='youtube-dl --write-description --write-info-json --write-sub --all-subs --sub-format "srt/best" --write-thumbnail --sleep-interval 1 --retries 30 -o "/media/serverhdd3/$NAME/%(playlist)s/%(title)s.%(ext)s" -f best -ciw "$URL"'
 
 mkdir /media/serverhdd3/"$NAME"
 echo '$COMMAND' > /media/serverhdd3/"$NAME"/readme.txt
