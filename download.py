d60cfbd0f765cb550b24db1cde0b05821f9dbb94
josephcappadona <josephcappadona27@gmail.com>
2019-01-07 13:52:08 -0700

    Updated argument structure, changed output format

--- download.py
+++ download.py
@@ -5,9 +5,10 @@ from os import makedirs, remove
 import pickle
 from sys import argv
 from pprint import pprint
+from vods_args import parse_args, create_handle
 
 
-def download_player_match(player, match, output_base_dir='./output'):
+def download_match(match, video_dir):
     
     youtube_url = match['youtube_url']
     p1, p2 = match['players']
@@ -23,15 +24,12 @@ def download_player_match(player, match, output_base_dir='./output'):
     try:
         yt = pytube.YouTube(youtube_url).streams.first()
         new_filename = safe_filename(new_title) + '.' + yt.mime_type.split('/')[-1]
-
-        output_dir = output_base_dir + '/' + player
-        makedirs(output_dir, exist_ok=True)
-        filepath = Path(output_dir + '/' + new_filename)
+        video_fp = Path(video_dir + '/' + new_filename)
         
         print('Downloading %s' % new_title)
-        if not filepath.is_file():
+        if not video_fp.is_file():
             try:
-                yt.download(output_dir, filename=new_title)
+                yt.download(video_dir, filename=new_title)
                 return True
             
             except (KeyboardInterrupt, OSError) as e:
@@ -39,7 +37,7 @@ def download_player_match(player, match, output_base_dir='./output'):
                     remove(str(filepath))
                 raise e
         else:
-            print('\tVideo already downloaded')
+            print('\tVideo already downloaded. Skipping...')
             
     except (pytube.exceptions.VideoUnavailable, OSError) as e:
         print('\nCould not download video (%s):' % str(type(e)).split("'")[1])
@@ -48,9 +46,9 @@ def download_player_match(player, match, output_base_dir='./output'):
     
     return False
         
-def load_player(player, data_dir='./output'):
+def load_matches(handle, data_dir='output'):
     try:
-        filename = '%s.pkl' % player
+        filename = '%s.pkl' % handle
         matches = pickle.load(open(data_dir + '/' + filename, 'rb'))
         return matches
         
@@ -59,68 +57,33 @@ def load_player(player, data_dir='./output'):
     
     
 if __name__ == '__main__':
-    if len(argv) < 3:
-        print('Usage:  python download.py [-p PLAYER | -P players.txt] [-c CHARACTER | -C characters.txt] [-t tournament] [-l limit]')
+
+    usage = 'Usage:  python download.py [-p PLAYER] [-c CHARACTER] [-t tournament]'
+    if len(argv) < 2:
+        print(usage)
         quit()
-    else:
-        parsed_args = enumerate(zip(argv[1:-1:2], argv[2::2]))
-        
-        players = set()
-        valid_characters = set()
-        valid_tournaments = set()
-        limit = 10**10
-        for i, (prefix, suffix) in parsed_args:
-            ind = 2 + i*2
-            
-            if prefix == '-p':
-                players.add(suffix)
-                
-            elif prefix == '-P':
-                with open(suffix, 'rt') as player_file:
-                    for player in player_file.read().split('\n'):
-                        if player:
-                            players.add(player)
-                
-            elif prefix == '-c':
-                valid_characters.add(suffix)
-                    
-            elif prefix == '-C':
-                with open(suffix, 'rt') as character_file:
-                    for character in character_file.read().split('\n'):
-                        if character:
-                            valid_characters.add(character)
-            
-            elif prefix == '-t':
-                valid_tournaments = tournaments.add(suffix)
-                
-            elif prefix == '-T':
-                with open(suffix, 'rt') as tournament_file:
-                    for tournament in tournament_file.read().split('\n'):
-                        if tournament:
-                            valid_tournaments.add(character)
-                    
-            elif prefix == '-l':
-                limit = int(suffix)
-                
-        for player in players:
-            print('\nDownloading VODs for %s\n' % player)
 
-            matches = load_player(player)
-            n_downloaded = 0
-            for match in matches:
-                
-                match_characters = set([character for character_list in match['characters'].values() for character in character_list])
-                
-                if (not valid_characters or all((character in match_characters) for character in valid_characters)) and (not valid_tournaments or tournament in valid_tournaments):
-                        
-                    downloaded = download_player_match(player, match)
-                    if downloaded:
-                        n_downloaded += 1
-                        if n_downloaded >= limit:
-                            print('Reached limit of %d' % limit)
-                            break
+    try:
+        players, characters, tournament = parse_args(argv)
+    except ValueError:
+        print('Arguments not formatted correctly.\n')
+        print(usage)
+        exit()
+    
+    data_dir = 'output'
+    file_handle = create_handle(players, characters, tournament)
+    video_dir = '%s/%s' % (data_dir, file_handle)
+    makedirs(video_dir, exist_ok=True)
 
-            print('\nFinished')
-            print('Videos written to %s' % ('./output/%s/' % player))
-            
-            
+    matches = load_matches(file_handle, data_dir=data_dir)
+    print('\nDownloading VODs for %s\n' % file_handle)
+    try:
+        for match in matches:
+            download_match(match, video_dir)
+    except KeyboardInterrupt:
+        print('Keyboard interrupt detected. Stopping script...')
+
+    print('\nFinished')
+    print('Videos written to \'%s\'' % video_dir)
+    
+    
